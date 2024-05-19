136f65effb9a9ff4bfd1c37bef4deac5552084c4
haroun-ab <hrnabd@hotmail.com>
2024-05-19 12:25:11 +0200

    home page finished with button, header & footer components

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './_pages/home/home.component';
-import { MainInformationsComponent } from './_pages/main-informations/main-informations.component';
-import { SocialMediaComponent } from './_pages/social-media/social-media.component';
-import { EducationComponent } from './_pages/education/education.component';
-import { ExperienceComponent } from './_pages/experience/experience.component';
-import { HardSkillsComponent } from './_pages/hard-skills/hard-skills.component';
-import { SoftSkillsComponent } from './_pages/soft-skills/soft-skills.component';
-import { HobbiesComponent } from './_pages/hobbies/hobbies.component';
-import { LanguagesComponent } from './_pages/languages/languages.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MainInformationsComponent } from './pages/main-informations/main-informations.component';
+import { SocialMediaComponent } from './pages/social-media/social-media.component';
+import { EducationComponent } from './pages/education/education.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { HardSkillsComponent } from './pages/hard-skills/hard-skills.component';
+import { SoftSkillsComponent } from './pages/soft-skills/soft-skills.component';
+import { HobbiesComponent } from './pages/hobbies/hobbies.component';
+import { LanguagesComponent } from './pages/languages/languages.component';
 
 const routes: Routes = [
   {path : "", component : HomeComponent},
--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -4,22 +4,22 @@ import * as fr from '@angular/common/locales/fr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {registerLocaleData} from "@angular/common";
-import { HomeComponent } from './_pages/home/home.component';
-import { MainInformationsComponent } from './_pages/main-informations/main-informations.component';
-import { SocialMediaComponent } from './_pages/social-media/social-media.component';
-import { EducationComponent } from './_pages/education/education.component';
-import { ExperienceComponent } from './_pages/experience/experience.component';
-import { HardSkillsComponent } from './_pages/hard-skills/hard-skills.component';
-import { SoftSkillsComponent } from './_pages/soft-skills/soft-skills.component';
-import { HobbiesComponent } from './_pages/hobbies/hobbies.component';
-import { LanguagesComponent } from './_pages/languages/languages.component';
-import { SectionComponent } from './_components/section/section.component';
-import { HeaderComponent } from './_components/header/header.component';
-import { FooterComponent } from './_components/footer/footer.component';
-import { RadioComponent } from './_components/radio/radio.component';
-import { TextAreaComponent } from './_components/text-area/text-area.component';
-import { InputComponent } from './_components/input/input.component';
-import { ButtonComponent } from './_components/button/button.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MainInformationsComponent } from './pages/main-informations/main-informations.component';
+import { SocialMediaComponent } from './pages/social-media/social-media.component';
+import { EducationComponent } from './pages/education/education.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { HardSkillsComponent } from './pages/hard-skills/hard-skills.component';
+import { SoftSkillsComponent } from './pages/soft-skills/soft-skills.component';
+import { HobbiesComponent } from './pages/hobbies/hobbies.component';
+import { LanguagesComponent } from './pages/languages/languages.component';
+import { SectionComponent } from './components/section/section.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RadioComponent } from './components/radio/radio.component';
+import { TextAreaComponent } from './components/text-area/text-area.component';
+import { InputComponent } from './components/input/input.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
